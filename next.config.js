d8b13b6e81ab3e4bca52e497280b67ecd66669fe
Mr. Utzig <lutzig.octavius@gmail.com>
2022-07-09 23:16:49 -0300

    Next.js config file setup

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  distDir: 'build',
   reactStrictMode: true,
   eslint: {
     dirs: ['src']
-  }
+  },
+  devIndicators: {
+    buildActivity: true
+  },
 }
 
 module.exports = nextConfig
