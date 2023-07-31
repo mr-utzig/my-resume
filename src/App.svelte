0ecb265876cd961db35c476b81be833e211a3f26
Mr. Utzig <lutzig.octavius@gmail.com>
2023-07-30 23:52:25 -0300

    feat: terminal, input and history components creation

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,44 @@
 <script lang="ts">
+	import TerminalInfo from "./lib/Terminal/Info.svelte";
+	import TerminalHistory from "./lib/Terminal/History.svelte";
+
+	let command: string = "";
+	const history: ICommandHistory[] = [
+		{
+			input: "cerveja",
+			response: "mim de papai",
+			path: "~/juca",
+		},
+	];
+
+	function inputFocus(): void {
+		document.getElementById("commandInput").focus();
+	}
 </script>
 
+<svelte:window on:click={inputFocus} />
 <main class="terminal">
-  <div class="user-input-history"></div>
-  <div class="user-input">
-    <span>mr-utzig@lutzig:~$ </span> <input type="text">
-  </div>
+	<TerminalHistory {history} />
+	<TerminalInfo>
+		<input
+			type="text"
+			id="commandInput"
+			bind:value={command}
+			autocomplete="off"
+		/>
+	</TerminalInfo>
 </main>
 
 <style>
+	.terminal {
+		width: 100%;
+		font-size: 1.4rem;
+	}
+	input {
+		flex: 1;
+		border: none;
+		outline: none;
+		background-color: transparent;
+		font-family: "FiraCode";
+	}
 </style>
