6c5cacb22dce5d739391989e8de7442bcfc76cfb
Luiz Utzig <luiz.utzig@messeminvestimentos.com.br>
2023-08-22 14:43:19 -0300

    feat: arrows working with the fake cursor

--- src/App.svelte
+++ src/App.svelte
@@ -3,15 +3,29 @@
 	import TerminalHistory from "./lib/Terminal/History.svelte";
 
 	let command: string = "";
-	let wd: string = "";
+	let wd: string = "~";
 	const history: ICommandHistory[] = [];
 
 	let hiddenInput: HTMLInputElement;
-	let inputCursor: HTMLSpanElement;
-	let inputCursorPosition: number = 0;
-	let inputCursorSize: number = 0.923;
+	let fakeInput: HTMLSpanElement;
+	let fakeInputCursor: HTMLSpanElement;
+	let fakeInputCursorSize: number = 0.923;
+	let fakeInputCursorPosition: number = 0;
 
-	function processCommandInput(e: KeyboardEvent): void {
+	let fakeInputCursorBackspace: number = 0;
+
+	// 15.691 -> 100
+	// 11.076 -> x
+
+	// 10.153 -> 100
+	// z      -> x
+
+	$: fakeInputSize = command.length * fakeInputCursorSize;
+
+	function inputControlKeyDown(e: KeyboardEvent): void {
+		fakeInput.style.backgroundColor = "transparent";
+		fakeInput.style.color = "#f1faff";
+
 		switch (e.key) {
 			case "Enter":
 				try {
@@ -28,8 +42,6 @@
 
 					console.log(input, command, flasgs, parameters);
 				} catch (e) {
-					console.log(e);
-
 					history.push({
 						input: e.input,
 						response: e.response,
@@ -41,41 +53,66 @@
 
 				break;
 			case "ArrowLeft":
-				console.log(
-					"LEFT",
-					command.length,
-					inputCursorSize,
-					command.length * inputCursorSize,
-					inputCursorPosition
-				);
-
-				if (command.length * inputCursorSize > Math.abs(inputCursorPosition)) {
-					inputCursorPosition -= inputCursorSize;
-					inputCursor.style.left = `${inputCursorPosition}rem`;
+				if (fakeInputSize > Math.abs(fakeInputCursorPosition)) {
+					fakeInputCursorPosition -= fakeInputCursorSize;
+					fakeInputCursor.style.left = `${fakeInputCursorPosition}rem`;
 				}
 
 				break;
 			case "ArrowRight":
-				console.log(
-					"RIGHT",
-					command.length,
-					inputCursorSize,
-					command.length * inputCursorSize,
-					inputCursorPosition
-				);
-
-				if ((command.length * inputCursorSize) > 0) {
-					inputCursorPosition += inputCursorSize;
-					inputCursor.style.left = `${inputCursorPosition}rem`;
+				if (fakeInputCursorPosition < 0) {
+					fakeInputCursorPosition += fakeInputCursorSize;
+					fakeInputCursor.style.left = `${fakeInputCursorPosition}rem`;
 				}
 
 				break;
 			case "ArrowUp":
+				fakeInputCursorPosition = fakeInputSize;
+				fakeInputCursor.style.left = `-${fakeInputCursorPosition}rem`;
+
 				break;
 			case "ArrowDown":
+				fakeInputCursorPosition = 0;
+				fakeInputCursor.style.left = `${fakeInputCursorPosition}rem`;
+
+				break;
+			case "a" || "A":
+				if (e.ctrlKey) {
+					fakeInput.style.backgroundColor = "#f1faff";
+					fakeInput.style.color = "#444";
+					fakeInputCursorPosition = 0;
+					fakeInputCursor.style.left = `${fakeInputCursorPosition}rem`;
+				}
+
 				break;
 			default:
+				break;
 		}
+
+		if (fakeInputCursorPosition < 0) {
+			fakeInputCursor.style.backgroundColor = "transparent";
+			fakeInputCursor.style.border = "1px solid #66d9ef";
+		} else {
+			fakeInputCursor.style.backgroundColor = "#66d9ef";
+			fakeInputCursor.style.border = "none";
+		}
+	}
+
+	function inputControlKeyUp(e: KeyboardEvent): void {
+		switch (e.key) {
+			case "Backspace":
+				break;
+			default:
+				break;
+		}
+
+		console.log(
+			e.key,
+			"Cursor pos:",
+			Math.abs(fakeInputCursorPosition),
+			"Input size:",
+			fakeInputSize
+		);
 	}
 
 	function focusOnHiddenInput(): void {
@@ -87,15 +124,16 @@
 <main class="terminal">
 	<TerminalHistory {history} />
 	<TerminalInfo {wd}>
-		<span class="fake-ci__input">{command}</span>
-		<span class="fake-ci__cursor" bind:this={inputCursor} />
+		<span class="fake-ci__input" bind:this={fakeInput}>{command}</span>
+		<span class="fake-ci__cursor" bind:this={fakeInputCursor} />
 	</TerminalInfo>
 	<input
 		type="text"
 		id="hiddenInput"
 		bind:value={command}
 		bind:this={hiddenInput}
-		on:keydown={processCommandInput}
+		on:keyup={inputControlKeyUp}
+		on:keydown={inputControlKeyDown}
 	/>
 </main>
 
@@ -105,17 +143,14 @@
 	}
 	.fake-ci__input {
 		white-space: pre;
-		background-color: aliceblue;
-		color: black;
+		/* background-color: aliceblue; */
+		/* color: black; */
 	}
 	.fake-ci__cursor {
 		position: relative;
 		box-sizing: border-box;
 		width: 0.923rem;
-		/* background-color: #66d9ef; */
-		background-color: transparent;
-		border: 1px solid #66d9ef;
-		/* margin-left: 0.1rem; */
+		background-color: #66d9ef;
 	}
 	input {
 		position: absolute;
